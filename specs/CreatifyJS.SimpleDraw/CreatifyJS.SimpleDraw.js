4351e798f5514c254437eab845471fdb1da3d36e
Joseph Chapman <joe@creatifyjs.com>
2012-03-13 15:04:56 +0000

    Renamed namespace, updated runner, outline for first test

---
+++ specs/CreatifyJS.SimpleDraw/CreatifyJS.SimpleDraw.js
@@ -0,0 +1,7 @@
+describe('CreatifyJS.SimpleDraw', function () {
+	var canvas = document.getElementsByTagName('canvas')[0];
+	var draw = null;
+	describe('Drawing a line', function () {
+		draw = new CreatifyJS.SimpleDraw(canvas);
+	});
+});
