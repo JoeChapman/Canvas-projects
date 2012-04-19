be629f62cc3efa893f41234f243580119805c470
Joseph Chapman <joe@creatifyjs.com>
2012-04-19 16:57:41 +0100

    Modified permittedKeys array to hash. Modified isPermittedKey fn to handle new format, updated tests.

--- scripts/CreatifyJS.Game/CreatifyJS.Game.js
+++ scripts/CreatifyJS.Game/CreatifyJS.Game.js
@@ -1,129 +1,140 @@
-var CreatifyJS = CreatifyJS || {};
+(function (window, undef) {
 
-CreatifyJS.Game = function (paused) {
-	this.canvas = document.getElementById('world');
-	this.moveDist = 3;
-	this.fps = 60;
-	this.player = new CreatifyJS.Player(this);
-	this.context = this.canvas.getContext('2d');
-	this.canvas_width = this.canvas.width;
-	this.canvas_height = this.canvas.height;
-	this.paused = false;
-	
-	this.eventDelegation();
-	this.play();
-	
-};
+	CreatifyJS.Game = function (options) {
+		this.canvas = options.canvas;
+		this.moveDist = options.moveDist;
+		this.fps = options.fps;
 
-CreatifyJS.Game.prototype.play = function () {
-	var game = this;
-	// start the game loop and store the timer
-	this.gameLoop = window.setInterval(
-		function () {
-			game.updateAll();
-			game.drawAll();
-		}, 
-		1000 / this.fps);
-
-	this.paused = false
-};
-
-CreatifyJS.Game.prototype.pause = function () {
-	window.clearInterval(this.gameLoop);
-	this.paused = true;
-};
-
-CreatifyJS.Game.prototype.reset = function () {
-	this.pause();
-	this.player.x = 100;
-	this.player.y = 100;
-	this.drawAll();
-};
-
-CreatifyJS.Game.prototype.eventDelegation = function () {
-	var game = this;
-	// Bind all keydown events to a callback
-	document.addEventListener('keydown', function (e) {
-		var key = e.keyCode;
+		this.context = this.canvas.getContext('2d');
+		this.canvas_width = this.canvas.width;
+		this.canvas_height = this.canvas.height;
+		this.paused = false;
+		this.player = new CreatifyJS.Player(this);
 		
-		if (game.paused && !CreatifyJS.Game.isPermittedKeyType('playing', key)) {
-			return;
-		}
-		// If this key is not one of our permitted keys, return!
-		if (!CreatifyJS.Game.isPermittedKeyType('permittedKeys', key)) { 
-			return;
-		}
-		// This key is allowed, so we have to prevent the default browser action
-		e.preventDefault();
-		// Delegate the key control
-		game.delegateKeyTypes.call(game, key);
-		
-	});
-};
+		this.eventDelegation(this.play())
+	};
 
-CreatifyJS.Game.prototype.delegateKeyTypes = function (key) {
-	if (CreatifyJS.Game.isPermittedKeyType('playing', key)) { 
-		this.delegateRunningControls(key);
-	}
-	if (CreatifyJS.Game.isPermittedKeyType('movement', key)) { 
-		this.player.update(this.getNewAxes(key));
-	}
-};
+	CreatifyJS.Game.prototype.play = function () {
+		var game = this;
+		// start the game loop and store the timer
+		this.gameLoop = window.setInterval(
+			function () {
+				game.updateAll();
+				game.drawAll();
+			}, 
+			1000 / this.fps);
 
-CreatifyJS.Game.prototype.delegateRunningControls = function (key) {
-	switch (key) {
-		case 80:
-			if (this.paused) {
-				this.play();
+		this.paused = false
+	};
+
+	CreatifyJS.Game.prototype.pause = function () {
+		window.clearInterval(this.gameLoop);
+		this.paused = true;
+	};
+
+	CreatifyJS.Game.prototype.reset = function () {
+		this.pause();
+		this.player.x = 100;
+		this.player.y = 100;
+		this.drawAll();
+	};
+
+	CreatifyJS.Game.prototype.updateAll = function () {
+		//this.player.update({x:2, y:2});
+	};
+
+	CreatifyJS.Game.prototype.drawAll = function () {
+		this.drawRectangle('#fff', 0, 0, this.canvas_width, this.canvas_height);
+		this.player.draw();
+	};
+
+	CreatifyJS.Game.prototype.drawRectangle = function (color, x, y, w, h) {
+		this.context.fillStyle = color;
+		this.context.fillRect(x, y, w, h);
+	};
+
+	CreatifyJS.Game.prototype.eventDelegation = function () {
+		var game = this;
+		// Bind all keydown events to a callback
+		document.addEventListener('keydown', function (e) {
+			var key = e.keyCode;	
+			if (game.paused && !CreatifyJS.Game.isPermittedKeyType('operations', key)) {
 				return;
 			}
-			this.pause();
-		break;
-		case 82:
-			this.reset();
-			return;
-		break;
-		default: 
-			return key;
-		break;
-	}
-};
+			// If this key is not one of our permitted keys, return!
+			if (!CreatifyJS.Game.isPermittedKeyType('any', key)) { 
+				return;
+			}
+			// This key is allowed, so we have to prevent the default browser action
+			e.preventDefault();
+			// Delegate the key control
+			game.delegateKeyTypes.call(game, key);
+		});
+	};
 
-CreatifyJS.Game.prototype.updateAll = function () {
-	//this.player.update({x:2, y:2});
-};
+	CreatifyJS.Game.prototype.delegateKeyTypes = function (key) {
+		if (CreatifyJS.Game.isPermittedKeyType('operations', key)) { 
+			this.delegatePlayingControls(key);
+		}
+		if (CreatifyJS.Game.isPermittedKeyType('movement', key)) { 
+			this.player.update(this.getNewAxes(key));
+		}
+	};
 
-CreatifyJS.Game.prototype.drawAll = function () {
-	this.drawRectangle('#fff', 0, 0, this.canvas_width, this.canvas_height);
-	this.player.draw();
-};
+	CreatifyJS.Game.prototype.delegatePlayingControls = function (key) {
+		switch (key) {
+			case 80:
+				if (this.paused) {
+					this.play();
+					return;
+				}
+				this.pause();
+			break;
+			case 82:
+				this.reset();
+				return;
+			break;
+			default: 
+				return key;
+			break;
+		}
+	};
 
-CreatifyJS.Game.prototype.drawRectangle = function (color, x, y, w, h) {
-	this.context.fillStyle = color;
-	this.context.fillRect(x, y, w, h);
-};
+	CreatifyJS.Game.prototype.getNewAxes = function (key) {
+		var axes = {x: 0, y: 0}, dist = this.moveDist;
+		switch (key) {
+			case 37: axes.x = -dist; break;
+			case 38: axes.y = -dist; break;
+			case 39: axes.x = +dist; break;
+			case 40: axes.y = +dist; break;
+		}
+		return axes;
+	};
 
-CreatifyJS.Game.prototype.getNewAxes = function (key) {
-	var axes = {x: 0, y: 0}, dist = this.moveDist;
+	// Put all keys here
+	CreatifyJS.Game.permittedKeys = {
+		movement: 	[37, 38, 39, 40], 
+		operations: [80, 82]
+	};
 
-	switch (key) {
-		case 37: axes.x = -dist; break;
-		case 38: axes.y = -dist; break;
-		case 39: axes.x = +dist; break;
-		case 40: axes.y = +dist; break;
-	}
-	return axes;
-};
+	CreatifyJS.Game.isPermittedKeyType = function (type, key) {
+		var permitted = CreatifyJS.Game.permittedKeys, typeKeys, a, len;
+		
+		if (type in permitted) {
+			// For types we know about
+			return permitted[type].indexOf(key) !== -1;
+		}
+		for (a in permitted) {
+			if (permitted.hasOwnProperty(a)) {
+				if (permitted[a].indexOf(key) !== -1) {
+					return true;
+				}
+			}
+		}
+		return false;
+		
+	};
 
-// Put all keys here
-CreatifyJS.Game.permittedKeys = [37, 38, 39, 40, 80, 82];
+	window.CreatifyJS = CreatifyJS;
 
-// Put arrow keys here
-CreatifyJS.Game.movement = [37, 38, 39, 40];
-
-CreatifyJS.Game.playing = [80, 82];
-
-// Have we put sanctions in place for this key??
-CreatifyJS.Game.isPermittedKeyType = function (arr, key) {
-	return CreatifyJS.Game[arr].indexOf(key) !== -1;
-};
+}(this));
--- specs/CreatifyJS.Game/CreatifyJS.Game.js
+++ specs/CreatifyJS.Game/CreatifyJS.Game.js
@@ -3,6 +3,13 @@ describe("Game", function() {
 	// A collection for resusing our spies
 	var spies = {};
 
+	// Our game options
+	var options = {
+		canvas: window.document.getElementById('world'),
+		moveDist: 3,
+		fps: 60
+	};
+
 	// HELPERS
 	var fireEvent = function (code, elem) {
 		var event = makeKeydownEvent();
@@ -19,14 +26,14 @@ describe("Game", function() {
 	describe("Creating a new game", function () {
 		it("Plays the game", function () {
 			spyOn(CreatifyJS.Game.prototype, "play");
-			var game = new CreatifyJS.Game();
+			var game = new CreatifyJS.Game(options);
 			expect(CreatifyJS.Game.prototype.play).toHaveBeenCalled();
 		});
 	});
 	describe("Playing the game", function () {
 		spies.spyDrawAll = spyOn(CreatifyJS.Game.prototype, "drawAll");
 		spies.spyUpdateAll = spyOn(CreatifyJS.Game.prototype, "updateAll");
-		var game = new CreatifyJS.Game();
+		var game = new CreatifyJS.Game(options);
 		waits(100);
 		it("Draws the game", function () {
 			expect(spies.spyDrawAll).toHaveBeenCalled();
@@ -38,7 +45,7 @@ describe("Game", function() {
 	describe("Drawing the game", function () {
 		spies.spyDrawRect = spyOn(CreatifyJS.Game.prototype, "drawRectangle");
 		spies.spyDrawPlayer = spyOn(CreatifyJS.Player.prototype, "draw");
-		var game = new CreatifyJS.Game();
+		var game = new CreatifyJS.Game(options);
 		waits(100);
 		it("Draws the world", function () {
 			expect(spies.spyDrawRect).toHaveBeenCalled();
@@ -48,22 +55,34 @@ describe("Game", function() {
 		});
 	});
 	describe("Pressing a key", function () {
-		var game = new CreatifyJS.Game();
+		var game = new CreatifyJS.Game(options);
 		describe("That's not a game key", function () {
-			var disallowedKey = 2;
+			var notGameKey = 2;
 			it("Does not affect the game", function () {
-				expect(CreatifyJS.Game.isPermittedKeyType('permittedKeys', disallowedKey)).toEqual(false);
+				expect(CreatifyJS.Game.isPermittedKeyType('any', notGameKey)).toEqual(false);
 			});
 		});
-		describe("That's a game key", function () {
-			var allowedKey = 39;
+		describe("That is a game key", function () {
+			var gameKey = 39;
 			it("Updates the game", function () {
-				expect(CreatifyJS.Game.isPermittedKeyType('permittedKeys', allowedKey)).toEqual(true);
+				expect(CreatifyJS.Game.isPermittedKeyType('any', gameKey)).toEqual(true);
+			});
+		});
+		describe("That is an arrow key", function () {
+			var arrowKey = 39;
+			it("Updates the game", function () {
+				expect(CreatifyJS.Game.isPermittedKeyType('movement', arrowKey)).toEqual(true);
+			});
+		});
+		describe("That is an operations key", function () {
+			var operationsKey = 82;
+			it("Updates the game", function () {
+				expect(CreatifyJS.Game.isPermittedKeyType('operations', operationsKey)).toEqual(true);
 			});
 		});
 	});
 	describe("Pressing the arrow keys", function () {
-		var game = new CreatifyJS.Game();
+		var game = new CreatifyJS.Game(options);
 		spies.update = spyOn(game.player, "update");
 		
 		describe("left", function () {
@@ -92,7 +111,7 @@ describe("Game", function() {
 		});
 	});
 	describe("Pressing 'p' when playing", function () {	
-		var game = new CreatifyJS.Game();
+		var game = new CreatifyJS.Game(options);
 		it("Pauses the game", function () {
 			expect(game.paused).toEqual(false);
 			fireEvent(80, game.canvas);
@@ -100,7 +119,7 @@ describe("Game", function() {
 		});
 	});
 	describe("Pressing 'p' when paused", function () {
-		var game = new CreatifyJS.Game();
+		var game = new CreatifyJS.Game(options);
 		it("Starts the game", function () {
 			expect(game.paused).toEqual(true);
 			fireEvent(80, game.canvas);
@@ -108,7 +127,7 @@ describe("Game", function() {
 		});
 	});
 	describe("Pressing 'r'", function () {
-		var game = new CreatifyJS.Game();
+		var game = new CreatifyJS.Game(options);
 		it("Moves the player to the starting position", function () {
 			// Move player
 			fireEvent(40, game.canvas);
